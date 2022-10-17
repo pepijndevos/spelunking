911ced9952fb135c425422799d31e1141ef032de
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 10:36:18 +0000

    Adding tarfile member sanitization to extractall()

--- spelunking.py
+++ spelunking.py
@@ -39,7 +39,26 @@ def download_repo(owner, repo, ref):
         flask.abort(e.code)
 
     with tarfile.open(None, 'r|gz', resp) as t:
-        t.extractall('source')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, "source")
 
 def get_latest(owner, repo, ref="master"):
     data = get_json("repos/%s/%s/git/refs/heads/%s" % (owner, repo, ref))
